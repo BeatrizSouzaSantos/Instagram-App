6003383ec8ae7e4084a83d88035fbb560cd4a5a0
Beatriz Souza Santos <beatrizsouzasantos1288@gmail.com>
2021-07-29 16:28:48 -0300

    Definindo a topbar

--- CoursePhotoApp/ContentView.swift
+++ CoursePhotoApp/ContentView.swift
@@ -9,22 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    VStack {
-        HStack{
-            Image(systemName: "camera")
-                //redimensionavel
-                .resizable()
-                //redimencionando
-                .frame(width: 25, height: 20)
-            //espacamento
-            Spacer()
-            Text("Instagram")
-            Spacer()
-            Text("Profile")
+        ZStack {
+            VStack {
+                NavigationBarView()
+                Spacer()
+            }
         }
-        //centralizando
-        .padding()
-    }
   }
 }
 struct ContentView_Previews: PreviewProvider {
@@ -33,3 +23,45 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+struct NavigationBarView: View {
+    var body: some View {
+        VStack {
+            HStack{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "camera")
+                        //redimensionavel
+                        .resizable()
+                        //redimencionando
+                        .frame(width: 25, height: 20)
+                }
+                .foregroundColor(.black)
+                //espacamento
+                Spacer()
+                Text("Instagram")
+                    .font(Font.custom("Billabong", size: 25))
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    //escolhendo a imagem atraves de assets
+                    Image(uiImage: #imageLiteral(resourceName: "WhatsApp Image 2021-07-29 at 10.55.44"))
+                        //renderizando a imagem
+                        .resizable()
+                        //renderizacao da imagem
+                        .renderingMode(.original)
+                        //consertando imagem
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 36, height: 36)
+                        //arredondando a imagem
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        //destacando a foto
+                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 10, x: 0, y: 10)
+                    
+                    
+                    
+                }
+            }
+            //centralizando
+            .padding()
+        }
+    }
+}
