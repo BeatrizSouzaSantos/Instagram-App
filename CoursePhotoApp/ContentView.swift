00cd0a4b7d737782f308f4a0c72a5db7a176b571
Beatriz Souza Santos <beatrizsouzasantos1288@gmail.com>
2021-07-29 15:09:11 -0300

    Adicionando icones

--- CoursePhotoApp/ContentView.swift
+++ CoursePhotoApp/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    VStack {
+        HStack{
+            Image(systemName: "camera")
+                //redimensionavel
+                .resizable()
+                //redimencionando
+                .frame(width: 25, height: 20)
+            //espacamento
+            Spacer()
+            Text("Instagram")
+            Spacer()
+            Text("Profile")
+        }
+        //centralizando
+        .padding()
     }
+  }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
